4781a21b5cdd7fbbd8b5bd3379286e76530618c2
Peter <zackyypee@gmail.com>
2025-08-09 23:20:32 +0100

    Updated Nav

--- src/components/NavSection.jsx
+++ src/components/NavSection.jsx
@@ -9,42 +9,53 @@ import { LiaTimesSolid } from "react-icons/lia";
 
 function NavSection(){
     const[barOpen,setBarOpen] = useState(false)
-    const toggleMenu = ()=> {
-        setBarOpen(!barOpen);
-    };
+
+    const navLinks = [
+        {label:"Home",link: "#" },
+        {label:"About",link: "#" },
+        {label:"Contact",link: "#"},
+        {label:"Collections",link: "#"},
+        {label:"Login",link: "#"},
+    ];
 
     return(
        <nav className="w-screen h-10 shadow-xl content-center top-0 left-0">
-        <div className="flex justify-between md:justify-around items-center ">
-            <div>ZS</div>
-            <div className="hidden md:flex">
-                <ul className="flex gap-10">
-                    <a href=""><li>Home</li></a>
-                    <a href=""><li>Shop</li></a>
-                    <a href=""><li>Men</li></a>
-                    <a href=""><li>Women</li></a>
-                    <a href=""><li>Kids</li></a>
-                </ul>
-            </div>
-            <div className="gap-10 hidden md:flex">
-                <div className=""> <CiSearch size={30} /> </div>
-                <div><GiShoppingCart size={30} /></div>
-            </div>
-            {barOpen &&(
-                <div onClick={toggleMenu}><LiaTimesSolid /></div>
-            )}
-            {!barOpen &&(
-               <div className="flex gap-5 md:hidden">
+        <div className="flex justify-between px-4 ">
+            {/* Logo */}
+            <div><a href="">ZS</a></div>
+            {/* Menubar */}
+            <div className="flex gap-2">
                 <div><GiShoppingCart size={30}/></div>
-                 <div onClick={toggleMenu}><IoMdMenu size={30}/></div>
-               </div>
+                <div>< IoMdMenu onClick={()=> setBarOpen(true)} size={30} className="cursor-pointer"/></div>   
+            </div>
 
-            )}
-        </div> 
+            {/* Side bar */}
+           
+          <div className={`h-screen w-screen fixed bg-black/50 top-0 right-0 md:hidden flex transition-all duration-300 ease-in
+             ${barOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}>
+
+             <section className={`h-[300px] w-56 text-black font-bold bg-white text-center flex-col ml-auto transform transition-transform duration-300 ease-in-out ${
+                barOpen ? "translate-x-0": "translate-x-full"}`}>
+            
+               <div className="flex flex-col gap-3  mt-10">
+                  <div>
+                    < LiaTimesSolid onClick={()=> setBarOpen(false)} size={30} className=" justify-end ml-24 mt-2 cursor-pointer" />
+                  </div>
+                  <div className="flex flex-col gap-3 mt-2">
+                    {navLinks.map( (d,i)=>(
+                  <a key={i} href="{d.link}" className="">{d.label}</a>
+                   ) )}
+                  </div>
+             </div>
+
+             </section>
+            </div>
+        </div>
+        
        </nav>
     )
 }
-
+ 
 
 
 
