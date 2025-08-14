2a5c7a83aa94384eaa43d4cb3498411655bada3b
khenok <adepateadeshola@gmail.com>
2025-08-14 16:47:23 -0700

    Add Trending page and update my footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,14 @@ import { RiInstagramFill } from "react-icons/ri"
 import { FaTwitter, FaLinkedin, FaArrowUp } from "react-icons/fa"
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import { Link } from 'react-scroll'
+
 
 
 const Footer = () => {
   useEffect(() => {
       AOS.init({
-        duration: 800,
+        duration: 1000,
         delay:200,
         once: false, })
     }, []); 
@@ -20,14 +22,15 @@ const Footer = () => {
             <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5"><span className="text-teal-400">Free</span> until you're ready to purchase</h1>
             <div>
                 {/**/}
-                <input type="text" placeholder="Enter your email" className="text-yellow-800 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 bg-white focus:outline-amber-50" />
-                <button className="bg-teal-400 hover:big-teal-500 duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full">Subscribe</button>
+                <input type="text" placeholder="Enter your email" className="text-black sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 bg-white focus:outline-amber-50" />
+                <button className="bg-teal-400 hover:bg-teal-700 hover:text-3xl duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full">Subscribe</button>
                 </div>
         </div>
 
     {/*footer container*/}
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
-     {/*footer section */}
+
+     {/*footer section  Brand name*/}
     <div>
         <h1 data-aos="zoom-in" className="text-2xl font-bold text-teal-200">ZS</h1>
         <p data-aos="zoom-in" data-aos-delay="200" className="mt-6">Your Premier destination for quality footwear. Step into comfort and style with our curated collection.</p>
@@ -47,8 +50,8 @@ const Footer = () => {
         </div>
 
         {/*footer quick-links */}
-    <div> <h1 data-aos="zoom-in" className="text-2xl font-bold text-teal-200">Quick Links</h1>
-    <ul className="mt-6">
+    <div> <h1 data-aos="zoom-in" data-aos-delay="600" className="text-2xl font-bold text-teal-200">Quick Links</h1>
+    <ul data-aos="zoom-in" data-aos-delay="700" className="quick-links mt-6 ">
         <li><a href="">About Us</a></li>
         <li><a href="">Contact Us</a></li>
         <li><a href="">Shipping Info</a></li>
@@ -58,8 +61,8 @@ const Footer = () => {
 
                 {/*footer category */}
 
-    <div><h1 data-aos="zoom-in" data-aos-delay="600" className="text-2xl font-bold text-teal-200">Categories</h1>
-     <ul className="mt-6">
+    <div><h1 data-aos="zoom-in" data-aos-delay="800" className="text-2xl font-bold text-teal-200">Categories</h1>
+     <ul data-aos="zoom-in" data-aos-delay="900" className="categories mt-6 ">
         <li><a href="">Men's Shoe</a></li>
         <li><a href="">Women's Shoe</a></li>
         <li><a href="">Kid's Shoe</a></li>
@@ -68,10 +71,40 @@ const Footer = () => {
         </ul>
     </div>
 
-     {/*footer category */}
-    <div>4</div>
-      
-    </div>    
+     {/*footer stay updated */}
+    <div>
+        <h1 data-aos="zoom-in" data-aos-delay="1000" className="text-2xl font-bold text-teal-200">Stay Updated</h1>
+        <p data-aos="zoom-in" data-aos-delay="200" className="mt-6">Subscribe to get special offers, free giveaways, and exclusive deals.</p>
+         <input type="text" placeholder="Enter your email" className="text-black mt-5 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 bg-white focus:outline-amber-50" />
+         <button className="bg-teal-400 hover:big-teal-500 mt-5 duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full hover:bg-teal-800 hover:text-3xl">Subscribe</button>
+
+        </div>
+     
+    </div>  
+      {/** Horizontal rule */}
+    <div className="border-gray-600 border-t-1 mx-5 "></div>  
+        {/**footer-policy */}
+        <div className="md:flex md:justify-between md:items-center sm:px-12 px-4  py-6">
+    
+        <div>
+            <p className="text-teal-300 text-[15px] leading-8">Copyright 2025, ZackShola, All Rights Reserved.</p>
+        </div>
+        {/** React Scroll */}
+     <div id="root" className="text-white p-3 rounded-full hover:bg-teal-700 cursor-pointer flex justify-center">
+      <Link to='root' spy={true} offset={-100} smooth={true}> <FaArrowUp className="size-7 hover:size-8.5" /></Link>
+    </div>
+
+    <div className="md:flex md:justify-between md:items-center flex mt-5">
+        <ul className="footer-policy flex gap-7 ">
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Service</a></li>
+            <li><a href="#">Cookies Policy</a></li>
+        </ul>
+    </div>
+
+    </div>
+    
+   
     </footer>
 
 )
